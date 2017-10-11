b5a764efc362879ddbd5ee3d4fac86c3e1cb5d8c
Pontus Wiberg <pontus@pontusw.com>
2017-10-11 10:44:52 +0300

    SO-576 check if already installed

--- files/setup-ruby.sh
+++ files/setup-ruby.sh
@@ -1,3 +1,7 @@
+if /usr/bin/ruby -V | grep "2.4*"; then
+	echo "Ruby already installed, exiting.."
+	exit 0
+fi
 cd /tmp
 wget http://ftp.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz
 tar -xzvf ruby-2.4.2.tar.gz
